d4408bc3e5b74c18356da195502e1d5fff79a3dd
ibanlopez <lopez.iban@gmail.com>
2021-09-17 15:07:09 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+// const path = require('path')
+
+const app = express()
+
+app.use(express.static('./public'))
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './index.html'))
+// })
+
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000...')
+})
